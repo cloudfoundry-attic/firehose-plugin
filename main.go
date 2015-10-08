b06b36f269e5b8165763165096bf002001864c8d
Johannes Tuchscherer <jtuchscherer@gmail.com>
2015-10-08 13:30:17 -0600

    Build version 0.6.0

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func (c *NozzlerCmd) GetMetadata() plugin.PluginMetadata {
 		Name: "FirehosePlugin",
 		Version: plugin.VersionType{
 			Major: 0,
-			Minor: 5,
+			Minor: 6,
 			Build: 0,
 		},
 		MinCliVersion: plugin.VersionType{
